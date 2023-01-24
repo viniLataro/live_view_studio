3da4468240b492ff9a7f3f08b27bbe1448614432
Vinicius Lataro <viniciusr.lataro@gmail.com>
2023-01-24 15:45:07 -0300

    add a countdown timer

--- lib/live_view_studio_web/live/license_live.ex
+++ lib/live_view_studio_web/live/license_live.ex
@@ -1,19 +1,40 @@
 defmodule LiveViewStudioWeb.LicenseLive do
   use LiveViewStudioWeb, :live_view
+  use Timex
 
   alias LiveViewStudio.Licenses
   import Number.Currency
 
   def mount(_params, _session, socket) do
-    socket = assign(socket, seats: 2, amount: Licenses.calculate(2))
+    if connected?(socket) do
+      :timer.send_interval(1000, self(), :tick)
+    end
+
+    expiration_time = Timex.shift(Timex.now(), hours: 1)
+
+    socket = assign(socket,
+      seats: 2,
+      amount: Licenses.calculate(2),
+      expiration_time: expiration_time,
+      time_remaining: time_remaining(expiration_time)
+    )
+
     {:ok, socket}
   end
 
   def render(assigns) do
     ~L"""
     <h1>Team License</h1>
+
     <div id="license">
       <div class="card">
+        <p class="m-4 font-semibold text-indigo-800">
+          <%= if @time_remaining > 0 do %>
+            <%= format_time(@time_remaining) %> left to save 20%
+          <% else %>
+            Expired!
+          <% end %>
+        </p>
         <div class="content">
           <div class="seats">
             <img src="images/license.svg">
@@ -47,4 +68,21 @@ defmodule LiveViewStudioWeb.LicenseLive do
       )
     {:noreply, socket}
   end
+
+  def handle_info(:tick, socket) do
+    expiration_time = socket.assigns.expiration_time
+    socket = assign(socket, time_remaining: time_remaining(expiration_time))
+    {:noreply, socket}
+  end
+
+  defp time_remaining(expiration_time) do
+    DateTime.diff(expiration_time, Timex.now())
+  end
+
+  defp format_time(time) do
+    time
+    |> Timex.Duration.from_seconds()
+    |> Timex.format_duration(:humanized)
+  end
+
 end
