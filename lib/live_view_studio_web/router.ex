89ae8ffd59a4809bce579ab6d0ce7a53c4aba90a
Vinicius Lataro <viniciusr.lataro@gmail.com>
2023-01-27 15:43:18 -0300

    live navigation

--- lib/live_view_studio_web/router.ex
+++ lib/live_view_studio_web/router.ex
@@ -27,6 +27,7 @@ defmodule LiveViewStudioWeb.Router do
     live("/flights", FlightsLive)
     live("/autocomplete", AutocompleteLive)
     live("/filter", FilterLive)
+    live("/servers", ServersLive)
   end
 
   # Other scopes may use custom stacks.
