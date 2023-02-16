e76f9520979559e99d850b5d62e0255f0329411a
Vinicius Lataro <viniciusr.lataro@gmail.com>
2023-02-16 10:12:36 -0300

    triggering controller actions

--- lib/live_view_studio_web/router.ex
+++ lib/live_view_studio_web/router.ex
@@ -65,7 +65,8 @@ defmodule LiveViewStudioWeb.Router do
   scope "/", LiveViewStudioWeb do
     pipe_through [:browser, :redirect_if_user_is_authenticated]
 
-    get "/users/register", UserRegistrationController, :new
+    live "users/register", RegisterLive, :new, as: :user_registration
+
     post "/users/register", UserRegistrationController, :create
     get "/users/log_in", UserSessionController, :new
     post "/users/log_in", UserSessionController, :create
